5d2542fed51f65d8101e5799e447076d5742eb14
mac rent <macrent@macs-MacBook-Air-2.local>
2021-06-11 10:38:10 +1000

    n/a

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import Profile from './profile/profile';
 import AdminDashboard from './AdminDashboard';
 import ProfileForm from './profile/profileForm';
